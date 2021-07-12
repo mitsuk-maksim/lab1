e91d60056742175c4ef913c04caaf05e6dbfea19
mitsuk-maksim <mitsuk.maksim@gmail.com>
2021-07-12 12:42:32 +0300

    Initial commit

---
+++ test_graphene.py
@@ -0,0 +1,27 @@
+import graphene
+
+class CreatePerson(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+
+    ok = graphene.Boolean()
+    person = graphene.Field(lambda: Person)
+
+    def mutate(root, info, name):
+        person = Person(name=name)
+        ok = True
+        return CreatePerson(person=person, ok=ok)
+
+class Person(graphene.ObjectType):
+    name = graphene.String()
+    age = graphene.Int()
+
+class MyMutations(graphene.ObjectType):
+    create_person = CreatePerson.Field()
+
+# We must define a query for our schema
+class Query(graphene.ObjectType):
+    person = graphene.Field(Person)
+
+schema = graphene.Schema(query=Query, mutation=MyMutations)
+print(schema)
